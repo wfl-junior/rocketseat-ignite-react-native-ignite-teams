095e2abe36739f74291485cdc54ea5c6576ad1a1
Wallace Júnior <wflj1997@hotmail.com>
2022-09-15 19:11:26 -0300

    Utilizando Styled Components

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,21 +1,7 @@
-import { StyleSheet, Text, View } from "react-native";
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#121214",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  title: {
-    color: "white",
-    fontWeight: "700",
-    fontSize: 36,
-  },
-});
+import { Container, Title } from "./styles";
 
 export const Groups: React.FC = () => (
-  <View style={styles.container}>
-    <Text style={styles.title}>Groups</Text>
-  </View>
+  <Container>
+    <Title>Groups</Title>
+  </Container>
 );
---
+++ src/screens/Groups/styles.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: #121214;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Title = styled.Text`
+  color: white;
+  font-weight: 700;
+  font-size: 36px;
+`;
