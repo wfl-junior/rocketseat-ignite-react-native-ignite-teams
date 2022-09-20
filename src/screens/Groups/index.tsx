d0d9ff501c99a52eb343c928ff41c51c4e1750ff
Wallace Júnior <wflj1997@hotmail.com>
2022-09-19 23:51:42 -0300

    Listando grupos

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,11 +1,23 @@
+import { useState } from "react";
+import { FlatList } from "react-native";
+import { GroupCard } from "~/components/GroupCard";
 import { Header } from "~/components/Header";
 import { Highlight } from "~/components/Highlight";
 import { Container } from "./styles";
 
-export const Groups: React.FC = () => (
-  <Container>
-    <Header />
+export const Groups: React.FC = () => {
+  const [groups, setGroups] = useState<string[]>([]);
 
-    <Highlight title="Turmas" subtitle="Jogue com a sua turma" />
-  </Container>
-);
+  return (
+    <Container>
+      <Header />
+      <Highlight title="Turmas" subtitle="Jogue com a sua turma" />
+
+      <FlatList
+        data={groups}
+        keyExtractor={group => group}
+        renderItem={({ item: group }) => <GroupCard title={group} />}
+      />
+    </Container>
+  );
+};
