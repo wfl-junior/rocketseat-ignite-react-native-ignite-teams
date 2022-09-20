30a82aed7fa42cc640b4dc480787ecdfc530ad33
Wallace Júnior <wflj1997@hotmail.com>
2022-09-19 22:10:09 -0300

    Fontes personalizadas

--- App.tsx
+++ App.tsx
@@ -1,21 +1,33 @@
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from "@expo-google-fonts/roboto";
 import { Fragment } from "react";
-import { StatusBar } from "react-native";
+import { ActivityIndicator, StatusBar } from "react-native";
 import { ThemeProvider } from "styled-components/native";
 import { Groups } from "~/screens/Groups";
 import { defaultTheme } from "~/themes";
 
-const App: React.FC = () => (
-  <Fragment>
-    <StatusBar
-      barStyle="light-content"
-      backgroundColor="transparent"
-      translucent
-    />
+const App: React.FC = () => {
+  const [isFontReady] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
 
-    <ThemeProvider theme={defaultTheme}>
-      <Groups />
-    </ThemeProvider>
-  </Fragment>
-);
+  return (
+    <Fragment>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
+      <ThemeProvider theme={defaultTheme}>
+        {isFontReady ? <Groups /> : <ActivityIndicator />}
+      </ThemeProvider>
+    </Fragment>
+  );
+};
 
 export default App;
