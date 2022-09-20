68ab1c15c42aeb2dadbc462576b61c1d238efd0a
Wallace Júnior <wflj1997@hotmail.com>
2022-09-20 02:07:21 -0300

    Listando os filtros

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,24 +1,48 @@
+import { useState } from "react";
+import { FlatList } from "react-native";
 import { Filter } from "~/components/Filter";
 import { Header } from "~/components/Header";
 import { Highlight } from "~/components/Highlight";
 import { IconButton } from "~/components/IconButton";
 import { Input } from "~/components/Input";
-import { Container, Form } from "./styles";
+import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
 
-export const Players: React.FC = () => (
-  <Container>
-    <Header showBackButton />
+const teams = ["Time A", "Time B"];
 
-    <Highlight
-      title="Nome da turma"
-      subtitle="Adicione a galera e separe os times"
-    />
+export const Players: React.FC = () => {
+  const [team, setTeam] = useState("Time A");
+  const [players, setPlayers] = useState<string[]>([]);
 
-    <Form>
-      <Input placeholder="Nome do participante" autoCorrect={false} />
-      <IconButton icon="add" />
-    </Form>
+  return (
+    <Container>
+      <Header showBackButton />
 
-    <Filter title="Time A" isActive />
-  </Container>
-);
+      <Highlight
+        title="Nome da turma"
+        subtitle="Adicione a galera e separe os times"
+      />
+
+      <Form>
+        <Input placeholder="Nome do participante" autoCorrect={false} />
+        <IconButton icon="add" />
+      </Form>
+
+      <HeaderList>
+        <FlatList
+          horizontal
+          data={teams}
+          keyExtractor={team => team}
+          renderItem={({ item }) => (
+            <Filter
+              title={item}
+              isActive={item === team}
+              onPress={() => setTeam(item)}
+            />
+          )}
+        />
+
+        <NumberOfPlayers>{players.length}</NumberOfPlayers>
+      </HeaderList>
+    </Container>
+  );
+};
--- src/screens/Players/styles.ts
+++ src/screens/Players/styles.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components/native";
+import styled, { css } from "styled-components/native";
 
 export const Container = styled.View`
   flex: 1;
@@ -14,4 +14,21 @@ export const Form = styled.View`
   align-items: center;
 
   border-radius: 6px;
+`;
+
+export const HeaderList = styled.View`
+  width: 100%;
+
+  flex-direction: row;
+  align-items: center;
+
+  margin: 32px 0 12px;
+`;
+
+export const NumberOfPlayers = styled.Text`
+  ${({ theme }) => css`
+    color: ${theme.colors.gray[200]};
+    font-size: ${theme.fontSize.sm};
+    font-family: ${theme.fontFamily.bold};
+  `}
 `;
